1f94c39a887d0e465f22eabaf5a2bddf9aa8ea2a
thesewer <luisadha@outlook.com>
2024-08-04 23:21:59 +0700

    Update fetchfollower.sh

--- fetchfollower.sh
+++ fetchfollower.sh
@@ -4,7 +4,7 @@ function fetchfollower() {
  eval 'cd $(dirname $0)'
  eval 'npm start'
 }
-fetchfollower || node <(curl -L https://raw.githubusercontent.com/thesewer/fetchFollower/main/index.mjs)
+fetchfollower 
 
 if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
   export -f fetchfollower
