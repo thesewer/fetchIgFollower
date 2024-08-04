e27a7c133eeca746d816f83219ef91aa23526b66
thesewer <luisadha@outlook.com>
2024-08-05 00:24:26 +0700

    Update fetchfollower.sh

--- fetchfollower.sh
+++ fetchfollower.sh
@@ -1,7 +1,8 @@
 #/usr/bin/sh
+# for Ztmexluis installation scripts
 
 function fetchfollower() {
- eval 'cd $(dirname $0) || cd ~/deps/fetchFollower/'
+ eval 'cd $(dirname $0)'
  eval 'npm start'
 }
 fetchfollower
