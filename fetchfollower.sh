a4ddc8cf7a1b360afe18252395ec99121c20e74e
thesewer <luisadha@outlook.com>
2024-08-04 23:33:17 +0700

    Update fetchfollower.sh

--- fetchfollower.sh
+++ fetchfollower.sh
@@ -1,10 +1,10 @@
 #/usr/bin/sh
 
 function fetchfollower() {
- eval 'cd $(dirname $0)'
+ eval 'cd $(dirname $0) || cd ~/deps/fetchFollower/'
  eval 'npm start'
 }
-fetchfollower 
+fetchfollower
 
 if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
   export -f fetchfollower
